9d07c33f111eaa5ae920748bc8dff23ac5b75c0a
Jamie Taffurelli <jltaffurelli@outlook.com>
2023-08-14 14:05:12 +0100

    added app insights

--- variables.tf
+++ variables.tf
@@ -23,6 +23,23 @@ variable "storage_account_name" {
   description = "Name of the Storage Account to deploy"
 }
 
+variable "app_insights" {
+  type = object({
+    name                                  = string
+    application_type                      = optional(string, "other")
+    daily_data_cap_in_gb                  = optional(number)
+    daily_data_cap_notifications_disabled = optional(bool, false)
+    retention_in_days                     = optional(number, 365)
+    sampling_percentage                   = optional(number, 100)
+    disable_ip_masking                    = optional(bool, false)
+    local_authentication_disabled         = optional(bool, false)
+    internet_ingestion_enabled            = optional(bool, true)
+    internet_query_enabled                = optional(bool, true)
+    force_customer_storage_for_profiler   = optional(bool, false)
+  })
+  description = "Name of the Storage Account to deploy"
+}
+
 /*variable "data_collection_rule_template_path" {
   type        = string
   description = "Path to data collection rule arm template"
